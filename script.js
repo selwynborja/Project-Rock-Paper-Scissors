45034bffe37a2ab780f3b8a88caf7a787ac72b36
John Selwyn Borja <selwynborja11@gmail.com>
2026-05-10 13:49:52 +0800

    Create computer choice function and return the appropriate move

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice(){
+  const randomNumber = Math.random();
+
+  if (randomNumber >= 0 && randomNumber < 1 / 3){
+    return 'Rock';
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3){
+    return 'Paper';
+  } else if (randomNumber > 2 / 3 && randomNumber <= 1){
+    return 'Scissors';
+  }
+}
+
+console.log(getComputerChoice());
+
