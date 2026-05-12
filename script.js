b3824cea5ffd638f92c8aa5d6f5735c4faf855e7
John Selwyn Borja <selwynborja11@gmail.com>
2026-05-12 21:21:04 +0800

    Logic for paper button

--- script.js
+++ script.js
@@ -22,3 +22,28 @@ function btnRock(){
   }
   alert(`You picked Rock. Computer picked ${computerMove}. ${result}`);
 }
+
+function btnPaper(){
+  const randomNumber = Math.random();
+  let computerMove = '';
+
+  if (randomNumber >= 0 && randomNumber < 1 / 3){
+    computerMove = 'Rock';
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3){
+    computerMove = 'Paper';
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1){
+    computerMove = 'Scissors';
+  }
+  console.log(computerMove);
+
+  let result = '';
+
+  if (computerMove === 'Rock'){
+    result = 'You win';
+  } else if (computerMove === 'Paper'){
+    result = 'Tie';
+  } else if (computerMove === 'Scissors'){
+    result = 'You lose';
+  }
+  alert(`You picked Paper. Computer picked ${computerMove}. ${result}`);
+}
