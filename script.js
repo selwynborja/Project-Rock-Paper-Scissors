b446b229a2dc1ea6c0b5e393987ceeccb942e3cb
John Selwyn Borja <selwynborja11@gmail.com>
2026-05-12 21:21:45 +0800

    Logic for scissors button

--- script.js
+++ script.js
@@ -47,3 +47,28 @@ function btnPaper(){
   }
   alert(`You picked Paper. Computer picked ${computerMove}. ${result}`);
 }
+
+function btnScissors(){
+  const randomNumber = Math.random();
+  let computerMove = '';
+
+  if (randomNumber >= 0 && randomNumber < 1 / 3){
+    computerMove = 'Rock';
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3){
+    computerMove = 'Paper';
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1){
+    computerMove = 'Scissors';
+  }
+  console.log(computerMove);
+
+  let result = '';
+
+  if (computerMove === 'Rock'){
+    result = 'You lose';
+  } else if (computerMove === 'Paper'){
+    result = 'You win';
+  } else if (computerMove === 'Scissors'){
+    result = 'Tie';
+  }
+  alert(`You picked Scissors. Computer picked ${computerMove}. ${result}`); 
+}
